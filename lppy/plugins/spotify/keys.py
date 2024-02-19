38c3bad74bec31478497d6913da9627e5933efab
Dominik Dlugajczyk <msocek@gmail.com>
2024-02-19 14:30:11 +0100

    Fix crashing when spotify is not running.

--- lppy/plugins/spotify/keys.py
+++ lppy/plugins/spotify/keys.py
@@ -1,3 +1,4 @@
+from gi.repository import GLib
 from pydbus import SessionBus
 
 from lppy.driver.action_resolvers import ScreenKey
@@ -5,29 +6,54 @@ from lppy.driver.action_resolvers import ScreenKey
 
 def get_spotify():
     bus = SessionBus()
-    proxy = bus.get("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
-    return proxy["org.mpris.MediaPlayer2.Player"]
+    try:
+        proxy = bus.get("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
+        return proxy["org.mpris.MediaPlayer2.Player"]
+    except GLib.GError:
+        return None
 
 
 class PlayPauseSK(ScreenKey):
     def get_paint_configuration(self):
         config = super().get_paint_configuration()
-        if get_spotify().PlaybackStatus == "Playing":
+        spotify = get_spotify()
+        if not spotify:
+            config["background"] = "grey"
+            return config
+
+        if spotify.PlaybackStatus == "Playing":
             config["background"] = "green"
         return config
 
     async def touch(self, x_axis: int, y_axis: int):
         await super().touch(x_axis, y_axis)
-        get_spotify().PlayPause()
+        spotify = get_spotify()
+        spotify and spotify.PlayPause()
 
 
 class PreviousSK(ScreenKey):
+    def get_paint_configuration(self):
+        config = super().get_paint_configuration()
+        spotify = get_spotify()
+        if not spotify:
+            config["background"] = "grey"
+        return config
+
     async def touch(self, x_axis: int, y_axis: int):
         await super().touch(x_axis, y_axis)
-        get_spotify().Previous()
+        spotify = get_spotify()
+        spotify and spotify.Previous()
 
 
 class NextSK(ScreenKey):
+    def get_paint_configuration(self):
+        config = super().get_paint_configuration()
+        spotify = get_spotify()
+        if not spotify:
+            config["background"] = "grey"
+        return config
+
     async def touch(self, x_axis: int, y_axis: int):
         await super().touch(x_axis, y_axis)
-        get_spotify().Next()
+        spotify = get_spotify()
+        spotify and spotify.Next()
